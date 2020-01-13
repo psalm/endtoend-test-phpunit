3abb0edb08d8e6e114009c0f0609d2974b23069b
orklah <ronan_morin@hotmail.com>
2020-01-13 07:28:28 +0100

    fix cs

--- src/Util/Annotation/DocBlock.php
+++ src/Util/Annotation/DocBlock.php
@@ -332,12 +332,11 @@ final class DocBlock
     {
         if (\defined($message) &&
             (\strpos($message, '::') !== false && \substr_count($message, '::') + 1 === 2)) {
-            
             $constant_value = \constant($message);
-            if(\is_string($constant_value)) {
+            if (\is_string($constant_value)) {
                 return $constant_value;
             }
-            
+
             return $message;
         }
 
